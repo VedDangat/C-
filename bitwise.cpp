121b1156bc5c72921e246f18a55c8488af282a1b
Ved Dangat <146609072+VedDangat@users.noreply.github.com>
2026-01-22 09:17:34 +0530

    Add files via upload

---
+++ bitwise.cpp
@@ -0,0 +1,96 @@
+/*
+#include<iostream>
+using namespace std;
+int main()
+{
+    //BITWISE OPERATOR 
+
+    //int a=3,b=7;
+      //cout<<(a&b)<<"\n";
+     //cout<<(a|b)<<"\n";
+     //cout<<(a^b)<<"\n";
+     //cout<<(4<<1)<<"\n";
+     //cout<<(10>>1)<<"\n";
+     //cout<<(8>>2)<<"\n";
+     //cout<<(6&10)<<"\n";
+     //cout<<(6|10)<<"\n";
+     //cout<<(6^10)<<"\n";
+     //cout<<(10<<2)<<"\n";
+     //cout<<(10>>1)<<"\n";
+
+     //OPERATOR PREEDENCE
+
+     //cout<<5*2+6-2/2<<"\n";
+     //cout<<9+(3*2/9)+2<<"\n";
+
+return 0;
+}
+
+
+
+         //SCOPE OF VARIABLE
+
+#include <iostream>
+using namespace std;
+
+int x = 10;   // Global variable
+
+void show() {
+    cout << x << endl;
+}
+
+int main() {
+    show();
+    cout << x << endl;
+    return 0;
+}
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int y = 5;   // Local variable
+    cout << y << endl;
+    return 0;
+}
+
+
+
+#include <iostream>
+using namespace std;
+
+int a = 20;   // Global variable
+
+void demo() {
+    int b = 10;   // Local variable
+    cout << "Global variable: " << a << endl;
+    cout << "Local variable: " << b << endl;
+}
+
+int main() {
+    demo();
+    return 0;
+}
+
+
+
+            //DATATYPE MODIFIERS
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"long="<<sizeof(long)<<"\n";
+    cout<<"short="<<sizeof(short)<<"\n";
+    cout<<"long long int="<<sizeof(long long int)<<"\n";
+    cout<<"signed="<<sizeof(signed)<<"\n";
+    cout<<"unsigned="<<sizeof(unsigned)<<"\n";
+
+
+    return 0;
+}
+
+
+
+*/
